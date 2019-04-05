2de7872b66d6553c568d26395520fa761d549575
showdown-fusions <49254255+showdown-fusions@users.noreply.github.com>
2019-04-05 08:33:00 -0400

    Create formats-data.js

---
+++ data/mods/fusions/formats-data.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**@type {{[k: string]: ModdedTemplateFormatsData}} */
+let BattleFormatsData = {
+	gumtei: {
+		randomBattleMoves: ["extremespeed", "sacredfire", "flareblitz", "uturn", "pursuit", "crunch", "stoneedge"],
+		randomDoubleBattleMoves: ["extremespeed", "sacredfire", "flareblitz", "uturn", "pursuit", "crunch", "stoneedge", "protect", "earthquake"],
+		tier: "OU",
+		doublesTier: "DOU",
+	},
+};
+
+exports.BattleFormatsData = BattleFormatsData;
