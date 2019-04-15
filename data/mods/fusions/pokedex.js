62fbce5f231aeb72b4d5a919b1f0b10f6bf98b66
showdown-fusions <49254255+showdown-fusions@users.noreply.github.com>
2019-04-14 21:37:32 -0400

    Add files via upload
    Added Stakanoir and fixed Gumtei Event

--- data/mods/fusions/pokedex.js
+++ data/mods/fusions/pokedex.js
@@ -16,6 +16,34 @@ let BattlePokedex = {
 		tier:"OU",
         isNonstandard:"Fusions"
 	},
+	stakanoirsteel: {
+		num: -2001,
+		species: "Stakanoir-Steel",
+		types: ["Ghost", "Steel"],
+		genderRatio: {M: 0.5, F: 0.5},
+		baseStats: {hp: 53, atk: 116, def: 173, spa: 59, spd: 118, spe: 29},
+		abilities: {0: "Beast Boost", 1: "Pressure", H: "Frisk"},
+		heightm: 3.85,
+		weightkg: 463.3,
+		color: "Grey",
+		eggGroups: ["Amorphous"],
+		tier:"OU",
+        isNonstandard:"Fusions"
+	},
+	stakanoirrock: {
+		num: -2001,
+		species: "Stakanoir-Rock",
+		types: ["Ghost", "Rock"],
+		genderRatio: {M: 0.5, F: 0.5},
+		baseStats: {hp: 53, atk: 116, def: 173, spa: 59, spd: 118, spe: 29},
+		abilities: {0: "Beast Boost", 1: "Pressure", H: "Frisk"},
+		heightm: 3.85,
+		weightkg: 463.3,
+		color: "Grey",
+		eggGroups: ["Amorphous"],
+		tier:"OU",
+        isNonstandard:"Fusions"
+	},
 };
 
 exports.BattlePokedex = BattlePokedex;
